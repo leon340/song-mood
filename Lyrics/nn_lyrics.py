af2fcc6afb17d5d064702ee464f1039a4abafe3b
Ethan Howes <edh5623@rit.edu>
2020-05-20 12:57:51 -0400

    Set up tf imports and data set

--- Lyrics/nn_lyrics.py
+++ Lyrics/nn_lyrics.py
@@ -4,4 +4,16 @@ Language: python3
 Author: Ethan David Howes <edh5623@rit.edu>
 Purpose: Build and train a machine learning based
 model to preform sentiment analysis on lyrics
+
+Model based on TensorFlow text classification tutorial:
+https://www.tensorflow.org/tutorials/text/text_classification_rnn
 """
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True, as_supervised=True)
+
+train_data, test_data = dataset['train'], dataset['test']
+
+encoder = info.features['text'].encoder
