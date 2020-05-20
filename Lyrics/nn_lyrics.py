9abc706aa5519d9df3310f010b6697c9721c8757
Ethan Howes <edh5623@rit.edu>
2020-05-20 11:32:22 -0400

    Initial commit to the sentiment_neural_net branch

---
+++ Lyrics/nn_lyrics.py
@@ -0,0 +1,7 @@
+"""
+File: nn_lyrics.py
+Language: python3
+Author: Ethan David Howes <edh5623@rit.edu>
+Purpose: Build and train a machine learning based
+model to preform sentiment analysis on lyrics
+"""
