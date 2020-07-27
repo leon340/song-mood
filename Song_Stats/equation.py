df44d5ba1761ceffe8861b8016c0ac7fc4537bb7
Ethan Howes <edh5623@rit.edu>
2020-07-27 11:59:20 -0400

    Completed research into correlations between valence and loudness and tempo. Updated equations.py with findings.

--- Song_Stats/equation.py
+++ Song_Stats/equation.py
@@ -17,7 +17,7 @@ and Audio Features. International Journal of Artificial Intelligence &amp; Appli
 doi:10.5121/ijaia.2015.6304
 
 Mode: 0.5
-Valence(Lyrics and Title): 1.0
+Valence(Lyrics and Title): 1.0 - maybe split this 75-25?
 Loudness: 0.7
 Tempo: 0.8
 
@@ -26,6 +26,8 @@ Mode is already given by Spotify as 1 being major and 0 being minor
 
 
 def sentiment(mode, lyric_sentiment, title_sentiment, loudness, tempo):
+    # 0.5(mode) + 0.75(lyric_sentiment) + 0.25(title_sentiment) + 0.7(loudness) + 0.8(tempo)   /   3
+    # 0.5(mode) + 1.0(words(lyric_sentiment, title_sentiment)) + 0.7(loudness) + 0.8(tempo)   /   3
     return None
 
 
@@ -35,6 +37,21 @@ def tempo(bpm):
     to the same with very happy song, that will be 1.0
     do it again with sad song, that will be 0.0
     now create a formula given bpm to put it somewhere in that 0.0-1.0 range
+
+    0.25 and 0.75 valences
+    All tempo happy total:  239
+    All tempo happy avg:  124.40260669456072
+    All tempo sad total:  213
+    All tempo sad avg:  109.87290140845074
+
+    0.9 and 0.10 valences
+    All tempo happy total:  70
+    All tempo happy avg:  126.98902857142858
+    All tempo sad total:  82
+    All tempo sad avg:  100.2535609756097
+
+    All tracks total:  1007
+    All tracks avg:  119.32066236345577
     :param bpm:
     :return:
     """
@@ -47,6 +64,21 @@ def loudness(dbs):
     to the same with very happy song, that will be 1.0
     do it again with sad song, that will be 0.0
     now create a formula given avg dbs to put it somewhere in that 0.0-1.0 range
+
+    0.25 and 0.75 valences
+    All  loudness happy total:  239
+    All  loudness tempo happy avg:  -7.873564853556487
+    All  loudness sad total:  213
+    All  loudness tempo sad avg:  -15.483995305164322
+
+    0.9 and 0.10 valences
+    All  loudness happy total:  70
+    All  loudness tempo happy avg:  -8.150214285714284
+    All  loudness sad total:  82
+    All  loudness tempo sad avg:  -21.327012195121956
+
+    All tracks total:  1007
+    All tracks avg  loudness :  -9.934176762661364
     :param dbs:
     :return:
     """
---
+++ Song_Stats/feature_research.py
@@ -0,0 +1,124 @@
+"""
+File: feature_research.py
+Language: python3
+Author: Ethan David Howes <edh5623@rit.edu>
+Purpose: Analyze features of songs in correlation
+to their valence
+"""
+
+import spotipy
+from secret import *
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Setup and authorize Spotify API
+auth_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+# Top 50 playlist
+top_playlist = sp.playlist("37i9dQZEVXbMDoHDwVN2tF")
+
+# Happy hits playlist
+happy_playlist = sp.playlist("37i9dQZF1DXdPec7aLTmlC")
+
+# Life sucks playlist
+sad_playlist = sp.playlist("37i9dQZF1DX3YSRoSdA634")
+
+# 80's Smash Hits
+eighties_playlist = sp.playlist("19PgP2QSGPcm6Ve8VhbtpG")
+
+# Jazz Classics
+jazz_playlist = sp.playlist('37i9dQZF1DXbITWG1ZJKYt')
+
+# Country's Greatest Hits: The '90s
+country_playlist = sp.playlist('37i9dQZF1DXaiEFNvQPZrM')
+
+# Classical Essentials
+classical_playlist = sp.playlist('37i9dQZF1DWWEJlAGA9gs0')
+
+# Rock Classics
+rock_playlist = sp.playlist('37i9dQZF1DWXRqgorJj26U')
+
+# I Love My '00s R&B
+rnb_playlist = sp.playlist('37i9dQZF1DWYmmr74INQlb')
+
+# Metal Essentials
+metal_playlist = sp.playlist('37i9dQZF1DWWOaP4H0w5b0')
+
+# Blues Classics
+blues_playlist = sp.playlist('37i9dQZF1DXd9rSDyQguIk')
+
+# Latin Pop Classics
+latin_playlist = sp.playlist('37i9dQZF1DX6ThddIjWuGT')
+
+all_tracks = top_playlist['tracks']['items'] + happy_playlist['tracks']['items'] + sad_playlist['tracks']['items'] \
+             + eighties_playlist['tracks']['items'] + jazz_playlist['tracks']['items'] + \
+             country_playlist['tracks']['items'] + classical_playlist['tracks']['items'] + \
+             rock_playlist['tracks']['items'] + rnb_playlist['tracks']['items'] + metal_playlist['tracks']['items'] \
+             + blues_playlist['tracks']['items'] + latin_playlist['tracks']['items']
+
+
+def avgAll(feature):
+    """
+    Finds the average of a given feature over the entire all_tracks data set
+    :param feature: the aspect of the song to average EX: Loudness, tempo
+    """
+    total = 0
+    avg = 0
+    for track in all_tracks:
+        total += 1
+        id = track['track']['id']
+        features = sp.audio_features(id)
+        tempo = features[0][feature]
+        avg += float(tempo)
+
+    print("All tracks total: ", total)
+    print("All tracks avg ", feature, ": ", avg/total)
+
+
+def analysisHappy(feature):
+    """
+    Finds the average of a given feature over happy songs in the all_tracks data set
+    :param feature: the aspect of the song to average EX: Loudness, tempo
+    """
+    total = 0
+    avg = 0
+    for track in all_tracks:
+        id = track['track']['id']
+        features = sp.audio_features(id)
+        val = features[0]['valence']
+        val = float(val)
+        if val >= 0.75:  # currently using valence of 0.75 as the 'happy' threshold
+            total += 1
+            tempo = features[0][feature]
+            avg += float(tempo)
+    print("All ", feature, "happy total: ", total)
+    print("All ", feature, "tempo happy avg: ", avg / total)
+
+
+def analysisSad(feature):
+    """
+    Finds the average of a given feature over sad songs in the all_tracks data set
+    :param feature: the aspect of the song to average EX: Loudness, tempo
+    """
+    total = 0
+    avg = 0
+    for track in all_tracks:
+        id = track['track']['id']
+        features = sp.audio_features(id)
+        val = features[0]['valence']
+        val = float(val)
+        if val <= 0.25:  # currently using valence of 0.75 as the 'sad' threshold
+            total += 1
+            tempo = features[0][feature]
+            avg += float(tempo)
+    print("All ", feature, "sad total: ", total)
+    print("All ", feature, "tempo sad avg: ", avg / total)
+
+
+analysisHappy('tempo')
+analysisSad('tempo')
+avgAll('tempo')
+
+analysisHappy('loudness')
+analysisSad('loudness')
+avgAll('loudness')
